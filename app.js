96d642aa2f8f0b12edc7d56035070b195e5e982d
Ugwu Chidalu <chidaludivinefavour72@gmail.com>
2024-11-07 00:52:06 +0100

    resposiveness over

---
+++ app.js
@@ -0,0 +1,19 @@
+function head() {
+    const heading = document.getElementById('h1');
+    h1.textContent = 'Digital Clock';
+}
+head();
+
+
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2,'0');//it is used to pad the current string to another string multiple times if needed until the required lenght is attained.
+    const minutes = now.getMinutes().toString().padStart(2,'0');
+    const seconds = now.getSeconds().toString().padStart(2,'0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+updateClock();
+setInterval(updateClock,1000);//it is a java script function that enables you to repeat the same thing multiple times according to the interval 1000millisecond=1sec.
+
+
