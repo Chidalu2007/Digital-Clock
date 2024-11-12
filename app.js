110f5e5a23a58a250cefd204c78b8b5adbfdd9f3
Ugwu Chidalu <chidaludivinefavour72@gmail.com>
2024-11-12 16:01:10 +0100

    icon was added

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-function head() {
-    const heading = document.getElementById('h1');
-    heading.textContent = 'Digital Clock';
-}
-head();
-
+const clockElement = document.getElementById('clock');
+const heading = document.getElementById('h1');
+heading.textContent = 'Digital Clock';
 function updateClock() {
-    const clockElement = document.getElementById('clock');
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2,'0');//it is used to pad the current string to another string multiple times if needed until the required lenght is attained.
-    const minutes = now.getMinutes().toString().padStart(2,'0');
-    const seconds = now.getSeconds().toString().padStart(2,'0');
+    const hours = now.getHours().toString().padStart(2, '0');//it is used to pad the current string to another string multiple times if needed until the required lenght is attained.
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
     clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
 updateClock();
-setInterval(updateClock,1000);//it is a java script function that enables you to repeat the same thing multiple times according to the interval 1000millisecond=1sec.
+setInterval(updateClock, 1000);//it is a java script function that enables you to repeat the same thing multiple times according to the interval 1000millisecond=1sec.
 
 
